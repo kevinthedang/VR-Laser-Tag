3bf51cb48830a6206c40cd44b82141751650985d
Vincent Do <thevinnycast@gmail.com>
2023-11-04 16:51:12 -0700

    Firing sound test

--- VR Laser Tag/Assets/FireLaser.cs
+++ VR Laser Tag/Assets/FireLaser.cs
@@ -8,12 +8,14 @@ public class FireLaser : MonoBehaviour
     public GameObject laserObj;
     public Transform spawn;
     public float speed = 20;
+    public AudioSource audioData;
 
     // Start is called before the first frame update
     void Start()
     {
         XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
         grabInteractable.activated.AddListener(Fire);
+        audioData = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,7 @@ public class FireLaser : MonoBehaviour
         GameObject laser = Instantiate(laserObj);
         laser.transform.eulerAngles = spawn.transform.eulerAngles;
         laser.transform.position = spawn.position;
+        audioData.Play();
         laser.GetComponent<Rigidbody>().velocity = spawn.forward * speed;
         Destroy(laser, 3);
     }
