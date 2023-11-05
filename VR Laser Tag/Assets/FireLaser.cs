626d94115d59570bd84200df20f4766305fa469a
Vincent D <51691886+VinnyVinVince@users.noreply.github.com>
2023-11-04 17:25:10 -0700

    Projectile Sounds and Collisions (#7)
    * Firing sound test
    
    * Audio falloff (spatial/3d)
    
    * Spatial audio fix
    
    * Lasers should destroy themselves on collision
    
    * Another laser gun
    
    * Moving projectile spawn point farther out
    
    * Lowered laser despawn time

--- VR Laser Tag/Assets/FireLaser.cs
+++ VR Laser Tag/Assets/FireLaser.cs
@@ -8,12 +8,14 @@ public class FireLaser : MonoBehaviour
     public GameObject laserObj;
     public Transform spawn;
     public float speed = 20;
+    public AudioSource audioData;
 
     // Start is called before the first frame update
     void Start()
     {
         XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
         grabInteractable.activated.AddListener(Fire);
+        audioData = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,7 +29,8 @@ public class FireLaser : MonoBehaviour
         GameObject laser = Instantiate(laserObj);
         laser.transform.eulerAngles = spawn.transform.eulerAngles;
         laser.transform.position = spawn.position;
+        audioData.Play();
         laser.GetComponent<Rigidbody>().velocity = spawn.forward * speed;
-        Destroy(laser, 3);
+        Destroy(laser, (float)1.5);
     }
 }
